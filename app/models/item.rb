56bdfc9b2ed6e62cc3a7a0bf656c3d367bf0f6d5
teriyakifilet <oresamasaikooo@gmail.com>
2021-03-12 07:00:04 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,7 @@ class Item < ApplicationRecord
   end
 
   with_options presence: true do
-    validates :item_image, :name, :detail, :price
+    validates :item_image, :name, :detail
   end
 
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
