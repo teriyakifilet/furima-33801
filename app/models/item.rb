334d022bb822a71cc5c104389a8f39a4ac038cb7
teriyakifilet <oresamasaikooo@gmail.com>
2021-03-12 04:52:00 +0900

    各カテゴリ実装（ActiveHash）

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,16 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :shipping_fee
+  belongs_to :prefecture
+  belongs_to :days_to_ship
+
   belongs_to :user
   has_one_attached :item_image
+
+  with_options numericality: { other_than: 1 } do
+    validates :category_id, :item_status_id, :shipping_fee_id, :prefecture_id, :days_to_ship_id
+  end
+
 end
