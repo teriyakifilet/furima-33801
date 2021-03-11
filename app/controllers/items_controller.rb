cef1d953287bc3f2e96f1f2addd09aad12dbc447
teriyakifilet <oresamasaikooo@gmail.com>
2021-03-12 07:13:48 +0900

    未ログイン遷移修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,7 +17,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to action: :index unless user_signed_in?
+    redirect_to new_user_session_path unless user_signed_in?
   end
 
   private
