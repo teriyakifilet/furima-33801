bd413bf13f932dad99d904035725abd1f3aff8e4
teriyakifilet <oresamasaikooo@gmail.com>
2021-03-12 07:00:32 +0900

    Itemモデル単体テスト実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,78 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+  
+  describe '商品出品機能' do
+    context '商品出品できる時' do
+      it '適切な情報が入力されていれば商品出品できる' do
+        expect(@item).to be_valid
+      end
+    end
+    context '商品出品できない時' do
+      it '商品画像がなければ商品出品できない' do
+        @item.item_image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item image can't be blank")
+      end
+      it '商品名がなければ商品出品できない' do
+        @item.name = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it '商品の説明がなければ商品出品できない' do
+        @item.detail = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Detail can't be blank")
+      end
+      it 'カテゴリーの情報がなければ商品出品できない' do
+        @item.category_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+      it '商品の状態についての情報がなければ商品出品できない' do
+        @item.item_status_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item status must be other than 1")
+      end
+      it '配送料の負担についての情報がなければ商品出品できない' do
+        @item.shipping_fee_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Shipping fee must be other than 1")
+      end
+      it '発送元の地域についての情報がなければ商品出品できない' do
+        @item.prefecture_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+      it '発送までの日数についての情報がなければ商品出品できない' do
+        @item.days_to_ship_id = 1
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Days to ship must be other than 1")
+      end
+      it '販売価格についての情報がなければ商品出品できない' do
+        @item.price = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+      it '販売価格が¥299以下では商品出品できない' do
+        @item.price = 299
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+      it '販売価格が¥10,000,000以上では商品出品できない' do
+        @item.price = 10000000
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+      it '販売価格が半角数字以外では商品出品できない' do
+        @item.price = "１０００"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+    end
+  end
+
 end
