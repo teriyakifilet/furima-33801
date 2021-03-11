9ef0755328e8ca3d20a5065ec408cb2dcb610e16
teriyakifilet <oresamasaikooo@gmail.com>
2021-03-12 04:54:29 +0900

    商品出品ページ実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,23 @@
 class ItemsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    elsif
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :detail, :category_id, :item_status_id, :shipping_fee_id, :prefecture_id, :days_to_ship_id, :price, :item_image).merge(user_id: current_user.id)
+  end
 end
