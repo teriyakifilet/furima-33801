8b806d0ca4eebb42867146e752b17fc72b77a549
teriyakifilet <oresamasaikooo@gmail.com>
2021-03-17 11:18:53 +0900

    rubocop実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except:[:index, :show] 
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :user_id_verification, only: [:edit, :update, :destroy]
 
