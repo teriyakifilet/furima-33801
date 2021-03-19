725bf07e5cebbcb5eb16e8a3268a4ac81eee438e
teriyakifilet <oresamasaikooo@gmail.com>
2021-03-19 11:31:32 +0900

    addressesテーブルの修正

--- db/migrate/20210319002434_create_addresses.rb
+++ db/migrate/20210319002434_create_addresses.rb
@@ -6,7 +6,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.integer     :prefecture_id,   null: false
       t.string      :city,            null: false
       t.string      :address,         null: false
-      t.string      :string
+      t.string      :building
       t.string      :phone_number,    null:false
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 2021_03_19_002434) do
     t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "address", null: false
-    t.string "string"
+    t.string "building"
     t.string "phone_number", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
