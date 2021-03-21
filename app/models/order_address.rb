458699f7d8da805c96bcc090cc567f5a3ff2596f
teriyakifilet <oresamasaikooo@gmail.com>
2021-03-21 12:00:09 +0900

    バリデーションの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,10 +4,10 @@ class OrderAddress
 
   with_options presence: true do
     validates :user_id, :item_id, :token, :city, :address
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "はハイフンを含めてください"}
-    validates :prefecture_id, numericality: {only_integer: true, other_than: 1, message: "を選択してください"}
-    validates :phone_number, format: {with: /\A[0-9]{11}\z/, message: "は11桁の数字で入力してください"}
   end
+  validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "が無効です"}
+  validates :prefecture_id, numericality: {only_integer: true, other_than: 1, message: "を選択してください"}
+  validates :phone_number, format: {with: /\A[0-9]{11}\z/, message: "が無効です"}
 
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
